55510a311c1ba41fff23c3b276640c157c1b1eec
Michael Jakob <mjakob478@gmail.com>
2026-03-31 23:10:00 -0500

    consolidating classes and optimizing layout

--- effects.js
+++ effects.js
@@ -1,20 +1,12 @@
-function sizeBgElement() {
-    const limit = document.getElementById("projects").offsetTop;
-    document.getElementById("hero").style.height = limit + 'px';
-}
-
-const observer = new IntersectionObserver(entries => {
-    entries.forEach( entry => {
-        if(entry.isIntersecting) {
-            entry.target.classList.add('visible');
-            //observer.unobserve(entry.target);
-        }
-        else{
-            entry.target.classList.remove('visible');
-        }
-    });
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("visible");
+      observer.unobserve(entry.target);
+    } else {
+      entry.target.classList.remove("visible");
+    }
+  });
 });
 
-document.querySelectorAll('.reveal').forEach(el => observer.observe(el))
-sizeBgElement();
-window.addEventListener('resize', sizeBgElement)
+document.querySelectorAll(".reveal").forEach((el) => observer.observe(el));
