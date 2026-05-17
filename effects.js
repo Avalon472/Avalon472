17af1539594ec5ffc65a8c73f298b63a824363d8
Michael Jakob <mjakob478@gmail.com>
2026-05-16 21:35:25 -0500

    Complete refactor of components and styling, changed dimensions of all images and filled in descriptions, changed observer to have higher threshold

--- effects.js
+++ effects.js
@@ -1,12 +1,15 @@
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("visible");
-      observer.unobserve(entry.target);
-    } else {
-      entry.target.classList.remove("visible");
-    }
-  });
-});
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+        observer.unobserve(entry.target);
+      } else {
+        entry.target.classList.remove("visible");
+      }
+    });
+  },
+  { threshold: 0.33 },
+);
 
 document.querySelectorAll(".reveal").forEach((el) => observer.observe(el));
